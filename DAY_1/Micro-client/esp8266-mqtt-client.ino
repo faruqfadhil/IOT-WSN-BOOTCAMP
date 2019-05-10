fcc1144f9cd4cb6ab14ed009cf0af46013090aa3
faruqfadhil <faruqfadhil7@gmail.com>
2019-05-10 13:14:33 +0700

    add esp client

---
+++ DAY_1/Micro-client/esp8266-mqtt-client.ino
@@ -0,0 +1,70 @@
+#include <ESP8266WiFi.h>
+#include <PubSubClient.h>
+ 
+const char* ssid = "wayfay";
+const char* password =  "frqfrqfrq";
+const char* mqttServer = "192.168.1.103";
+const int mqttPort = 1883;
+const char* mqttUser = "seecow";
+const char* mqttPassword = "frq03051997";
+ 
+WiFiClient espClient;
+PubSubClient client(espClient);
+ 
+void setup() {
+ 
+  Serial.begin(115200);
+ 
+  WiFi.begin(ssid, password);
+ 
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.println("Connecting to WiFi..");
+  }
+  Serial.println("Connected to the WiFi network");
+ 
+  client.setServer(mqttServer, mqttPort);
+  client.setCallback(callback);
+ 
+  while (!client.connected()) {
+    Serial.println("Connecting to MQTT...");
+ 
+    if (client.connect("ESP8266Client", mqttUser, mqttPassword )) {
+ 
+      Serial.println("connected");  
+ 
+    } else {
+ 
+      Serial.print("failed with state ");
+      Serial.print(client.state());
+      delay(2000);
+ 
+    }
+  }
+ 
+  
+  client.subscribe("esp/test");
+ 
+}
+ 
+void callback(char* topic, byte* payload, unsigned int length) {
+ 
+  Serial.print("Message arrived in topic: ");
+  Serial.println(topic);
+ 
+  Serial.print("Message:");
+  for (int i = 0; i < length; i++) {
+    Serial.print((char)payload[i]);
+  }
+ 
+  Serial.println();
+  Serial.println("-----------------------");
+ 
+}
+ 
+void loop() {
+  client.publish("esp/test", "Hello from ESP8266");
+  Serial.println("published");
+  client.loop();
+  delay(5000);
+}
